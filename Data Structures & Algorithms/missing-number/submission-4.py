df0f97aa08d8d5bdf847d21489fe8bdd7e2f65df
Pooja Pandey <poojapandey2308@gmail.com>
2026-04-30 21:31:54 +0100

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        x1 = 0
+        x2 = 0
+        n=len(nums)
+        # XOR all elements in array
+        for i in range(len(nums)):
+            x1 = x1 ^ nums[i]
+        # XOR all numbers from 1 to n
+        for i in range(n+1):
+            x2 = x2 ^ i
+        return x1 ^ x2
