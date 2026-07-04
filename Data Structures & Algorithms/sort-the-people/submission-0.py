0af8f4360ca8ddc37bc2e5f5616500c86d5a5965
Pooja Pandey <poojapandey2308@gmail.com>
2026-07-04 16:04:53 +0100

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        mapp = dict()
+        res = []
+        for i in range(len(names)):
+            mapp[heights[i]] = names[i]
+
+        for k, v in sorted(mapp.items(), key=lambda item: item[0], reverse=True):
+            res.append(v)
+        
+        return res
+
+
+
+            
+
+
+
+        
