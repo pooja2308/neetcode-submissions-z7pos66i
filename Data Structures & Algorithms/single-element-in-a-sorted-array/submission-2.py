cae82b4152cbf3c838ab605b5f1ee8b411d03940
Pooja Pandey <poojapandey2308@gmail.com>
2026-08-01 21:22:45 +0100

    Add: single-element-in-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if mid % 2 == 1: # which means index is not even from the pair
+                mid -= 1
+            if nums[mid] == nums[mid + 1]: # perfect pair so move l 
+            # to start of next pair
+                l = mid + 2
+            else:
+                r = mid #single element could be mid or before mid
+        return nums[l]
+            
+        
